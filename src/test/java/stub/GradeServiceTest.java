624bdc3c948d4732394477cb6729811eec378fc2
unknown <hzh_innovation@163.com>
2020-08-04 11:58:27 +0800

    [hzh] feat: remove some unuse import and format code

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -11,8 +11,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
